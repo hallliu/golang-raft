74639bacdbc71b166fa083f410d2466770e2e321
Hall Liu <hallliu000@gmail.com>
2015-04-03 19:49:59 -0500

    implemented heartbeat function

--- raft_core/raft.go
+++ raft_core/raft.go
@@ -1,6 +1,7 @@
 package raft_core
 
 import (
+	"encoding/json"
 	"math/rand"
 	"raft/transporter"
 	"time"
@@ -40,5 +41,43 @@ func (node *RaftNode) run() {
 	}
 }
 
-func (node *RaftNode) handleTimeout()                             {}
+func (node *RaftNode) handleTimeout() {
+	switch {
+	case node.currentRole == clusterFollower:
+		node.beginCandidacy()
+	case node.currentRole == leaderCandidate:
+		node.beginCandidacy()
+	case node.currentRole == clusterLeader:
+		node.heartBeat()
+	}
+}
+
+// heartBeat sends a heartbeat message to all the followers.
+// This function prevents followers' election timeouts from expiring.
+func (node *RaftNode) heartBeat() {
+	for _, hostname := range node.hostnames {
+		prevLogIndex := node.nextIndex[hostname] - 1
+		lastLogTerm := node.messageLog[prevLogIndex].term
+
+		heartBeatCommand := appendEntriesCmd{
+			commandType:       appendEntriesType,
+			term:              node.currentTerm,
+			leaderId:          node.serverId,
+			prevLogIndex:      prevLogIndex,
+			prevLogTerm:       lastLogTerm,
+			entries:           node.messageLog[prevLogIndex+1:],
+			leaderCommitIndex: node.commitIndex,
+		}
+		heartBeatJson, _ := json.Marshal(heartBeatCommand)
+
+		heartBeatMessage := transporter.Message{
+			Destination: hostname,
+			Source:      node.serverId,
+			Command:     heartBeatJson,
+		}
+
+		node.MsgTransport.Send <- &heartBeatMessage
+	}
+}
+
 func (node *RaftNode) handleMessage(message *transporter.Message) {}
